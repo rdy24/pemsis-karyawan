8fd9759e285f0395a4b4e386c3d0c2d949827321
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2022-10-21 18:11:17 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top bg-white mb-5">
+        <div class="container">
+            <a class="navbar-brand fw-semibold text-primary" href="#">PENERIMAAN KARYAWAN</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item py-2 px-2">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item py-2 px-2">
+                        <a class="nav-link" aria-current="page" href="#">Features</a>
+                    </li>
+                    <li class="nav-item py-2 px-2">
+                        <a class="nav-link" aria-current="page" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item py-2 px-2">
+                        <a class="nav-link" aria-current="page" href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <div class="row mt-5">
+            <div class="col align-self-center align-items-center mt-5">
+                <h1 class="fw-semibold mt-5">Penerimaan Karyawan</h1>
+                <p class="fw-light" data-aos="fade-up" data-aos-delay="800">Pengolahan data penerimaan karyawan dengan
+                    metode Simple Additiv Weighting. Perusahaan harus melaksanakan proses perekrutan dan seleksi
+                    karyawan yang selektif sehingga sesuai dengan syarat kualifikasi dan kompetensi yang dibutuhkan oleh
+                    perusahaan.</p>
+                <a href="/dashboard" class="btn btn-primary col-3" data-aos="fade-up" data-aos-delay="1200">Login</a>
+            </div>
+            <div class="col d-none d-sm-block mt-5">
+                <img src="image/undraw_Font_re_efri.png" class="img-fluid mt-5" data-aos="fade-left"
+                    data-aos-delay="1600">
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
